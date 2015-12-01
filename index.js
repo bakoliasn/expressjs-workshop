92e03dbab97f5c39e351d18bc263fb913e32f707
bakoliasn <bakoliasn@gmail.com>
2015-12-01 20:09:01 +0000

    6th exercise

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 var entries = [{firstName:'steve', lastName:'smith'},{firstName:'john', lastName:'doe'}];
 
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   var returning = [];
-for (var i = 0; i < entries.length; i++){
-  if(req.query.name === entries[i].firstName || req.query.name === entries[i].lastName){
-    returning.push(entries[i]);
-    console.log(returning);
+  for (var i = 0; i < entries.length; i++){
+    if(req.query.name === entries[i].firstName || req.query.name === entries[i].lastName){
+      returning.push(entries[i]);
+      console.log(returning);
+    }
   }
-}
   res.json(returning);
+});
+
+
+app.post('/entries', function(req, res){
+  console.log(req.body);
+  res.json(req.body);
 });
 
 var server = app.listen(process.env.PORT, process.env.IP, function () {
